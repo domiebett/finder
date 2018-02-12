20a36fc5484be44f99562bd8b78d384a53612fff
DomieBett <dbett49@gmail.com>
2018-02-12 16:46:31 +0300

    chore(modal): redesign modals and auth button
    This commit redesigns the modals in the applications to make them larger.
    It also transfers the login button to the top right corner of the
    screen which is to be replaced by the user name once user has logged in
    succesfully.
    
    It also adds the option to register on the login modal, and to login
    on the register modal.
    
    [Delivers 155120497]

--- src/app/modules/items/dashboard/dashboard.component.ts
+++ src/app/modules/items/dashboard/dashboard.component.ts
@@ -1,46 +1,17 @@
-import { AuthService } from './../../../services/auth.service';
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ModalService } from '../../../services/modal.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit, OnDestroy {
+export class DashboardComponent implements OnInit {
 
   currentUser;
-  subscription;
 
   constructor(
-    private modalService: ModalService,
-    private authService: AuthService
-  ) {
-    this.currentUser = this.authService.currentUser;
-    this.subscription = authService.currentUserChange
-      .subscribe((user) => {
-        this.currentUser = user;
-      });
-  }
+  ) { }
 
   ngOnInit() {
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
-
-  /**
-   * Opens the authentication login modal
-   */
-  openLoginModal() {
-    this.modalService.openLoginModal();
-  }
-
-  /**
-   * Opens the authentication register modal
-   */
-  openRegisterModal() {
-    this.modalService.openRegisterModal();
   }
 }
